5535b0e7c422ee38b5e42fc714ad774b863153a4
enkronan <robin@tempura.se>
2020-05-01 13:19:44 +0200

    type: refactor
    added hovereffect which also adds images to .distortion div

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: './img/fashion1.jpg',
+    image2: './img/fashion2.jpg',
+    displacementImage: "./diss.png"
+});

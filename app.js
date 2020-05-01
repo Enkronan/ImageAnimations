bec059ef8d0a2d28abf4e8cecaec331eb575e6bc
enkronan <robin@tempura.se>
2020-05-01 13:25:33 +0200

    type: refactor
    changed the link for hoverEffect displacement image

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ new hoverEffect({
     intensity: 0.2,
     image1: './img/fashion1.jpg',
     image2: './img/fashion2.jpg',
-    displacementImage: "./diss.png"
+    displacementImage: "./img/heightMap.png"
 });
